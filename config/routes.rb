a215f13b63530cc52d72e5d10bceff6ddad237ac
Jorge Valdivia <jvaldivia@rarestep.com>
2015-03-25 16:25:55 -0600

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  root 'dashboard#show'
+  root 'dashboards#show'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
